bfda904944eeb6b2b2311ae2e9a226052befaf82
aaron-sisk <debtdota16@gmail.com>
2024-07-11 11:02:20 -0400

    add function getComputerChoice

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 console.log("Hello World!")
+
+function getComputerChoice() {
+    let a = Math.random();
+    if (a >= 0.66) {
+        return "Rock"
+    }
+    else if (a < 0.66 && a > 0.33) {
+        return "Paper"
+    }
+    else {
+        return "Scisscors"
+    }
+}
+
+console.log(getComputerChoice())
